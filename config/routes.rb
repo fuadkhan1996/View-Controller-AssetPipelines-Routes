5f7dba2d009334ef2d6d6ad6b0d273e0139b477d
fuadkhan1996 <fuad.khan@thedevden.co>
2019-11-22 10:20:02 +0500

    Finish layout and routes

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  #get 'users/new'
+  get 'static_pages/home'
+
+  get '/signup', to: 'users#new'
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
+  # post '/signup', to: 'static_pages#signup'
+  # post '/login', to: 'static_pages#login'
+  
+
+  root 'static_pages#home'
+end
